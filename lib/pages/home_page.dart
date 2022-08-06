433ecfc17e590747942f1ec99c83bdfe390fa1f6
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-06 11:28:15 -0500

    HomePage 1.3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_vacunapp/ui/general/colors.dart';
+import 'package:flutter_codigo_vacunapp/ui/widgets/item_list_widget.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
@@ -35,94 +36,11 @@ class HomePage extends StatelessWidget {
                 color: kFontPrimaryColor.withOpacity(0.85),
               ),
             ),
-            Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(14.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
-                    blurRadius: 12,
-                    offset: const Offset(4, 4),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/user.svg',
-                              height: 14.0,
-                              color: kFontPrimaryColor.withOpacity(0.7),
-                            ),
-                            const SizedBox(
-                              width: 4.0,
-                            ),
-                            Text(
-                              "Nombre completo",
-                              style: TextStyle(
-                                fontSize: 13.0,
-                                color: kFontPrimaryColor.withOpacity(0.7),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          "Fiorella de Fátima Montes",
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: kFontPrimaryColor.withOpacity(0.90),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 4.0,
-                        ),
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/id-card.svg',
-                              height: 14.0,
-                              color: kFontPrimaryColor.withOpacity(0.7),
-                            ),
-                            const SizedBox(
-                              width: 4.0,
-                            ),
-                            Text(
-                              "DNI",
-                              style: TextStyle(
-                                fontSize: 13.0,
-                                color: kFontPrimaryColor.withOpacity(0.7),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          "42322211",
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: kFontPrimaryColor.withOpacity(0.90),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: SvgPicture.asset(
-                      'assets/icons/link.svg',
-                      color: kFontPrimaryColor.withOpacity(0.9),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            ItemListWidget(),
+            ItemListWidget(),
+            ItemListWidget(),
+            ItemListWidget(),
+            ItemListWidget(),
           ],
         ),
       ),
---
+++ lib/ui/widgets/item_list_widget.dart
@@ -0,0 +1,101 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo_vacunapp/ui/general/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ItemListWidget extends StatelessWidget {
+  const ItemListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:
+      const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(14.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 12,
+            offset: const Offset(4, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/user.svg',
+                      height: 14.0,
+                      color: kFontPrimaryColor.withOpacity(0.7),
+                    ),
+                    const SizedBox(
+                      width: 4.0,
+                    ),
+                    Text(
+                      "Nombre completo",
+                      style: TextStyle(
+                        fontSize: 13.0,
+                        color: kFontPrimaryColor.withOpacity(0.7),
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  "Fiorella de Fátima Montes",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: kFontPrimaryColor.withOpacity(0.90),
+                  ),
+                ),
+                const SizedBox(
+                  height: 4.0,
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/id-card.svg',
+                      height: 14.0,
+                      color: kFontPrimaryColor.withOpacity(0.7),
+                    ),
+                    const SizedBox(
+                      width: 4.0,
+                    ),
+                    Text(
+                      "DNI",
+                      style: TextStyle(
+                        fontSize: 13.0,
+                        color: kFontPrimaryColor.withOpacity(0.7),
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  "42322211",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: kFontPrimaryColor.withOpacity(0.90),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/icons/link.svg',
+              color: kFontPrimaryColor.withOpacity(0.9),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
