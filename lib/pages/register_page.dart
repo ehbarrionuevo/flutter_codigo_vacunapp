53f5cde57dea0377f034462c9545c3bda1575489
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-07 11:47:09 -0500

    Register Page 3.0

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_vacunapp/ui/general/colors.dart';
+import 'package:flutter_codigo_vacunapp/ui/widgets/button_normal_widget.dart';
 import 'package:flutter_codigo_vacunapp/ui/widgets/textfield_normal_widget.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -28,49 +29,62 @@ class RegisterPage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(14.0),
-          child: Column(
-            children: [
-              Text(
-                "Registrar nuevo carnert",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.w600,
-                  color: kFontPrimaryColor.withOpacity(0.85),
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    "Registrar nuevo carnert",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w600,
+                      color: kFontPrimaryColor.withOpacity(0.85),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  TextFieldNormalWidget(
+                    hintText: "Nombre completo",
+                    icon: 'user',
+                    isDNI: false,
+                  ),
+                  TextFieldNormalWidget(
+                    hintText: "DNI",
+                    icon: 'id-card',
+                    isDNI: true,
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  Text(
+                    "Carnet QR",
+                    style: TextStyle(
+                        color: kFontPrimaryColor.withOpacity(0.75),
+                        fontSize: 15.0),
+                  ),
+                  QrImage(
+                    data: url,
+                    version: QrVersions.auto,
+                    size: 220.0,
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 12.0,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ButtonNormalWidget(
+                text: "Registrar carnet",
+                icon: 'check',
+                onTap: (){
+
+                },
               ),
-              TextFieldNormalWidget(
-                hintText: "Nombre completo",
-                icon: 'user',
-                isDNI: false,
-              ),
-              TextFieldNormalWidget(
-                hintText: "DNI",
-                icon: 'id-card',
-                isDNI: true,
-              ),
-              const SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                "Carnet QR",
-                style: TextStyle(
-                  color: kFontPrimaryColor.withOpacity(0.75),
-                  fontSize: 15.0
-                ),
-              ),
-              QrImage(
-                data: url,
-                version: QrVersions.auto,
-                size: 220.0,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
