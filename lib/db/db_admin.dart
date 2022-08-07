300c3b5d5085f73b2cb2ff92fb1f3295c9df28b8
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-07 12:14:03 -0500

    Database 1

---
+++ lib/db/db_admin.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBAdmin {
+
+  Database? _myDatabase;
+
+  static final DBAdmin db = DBAdmin._();
+  DBAdmin._();
+
+
+
+  Future<Database?> checkDatabase() async {
+    if (_myDatabase != null) {
+      return _myDatabase;
+    }
+    _myDatabase = await initDatabase();
+    return _myDatabase;
+  }
+
+  Future<Database> initDatabase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, "LicenseDB.db");
+    return openDatabase(
+      path,
+      version: 1,
+      onOpen: (db){},
+      onCreate: (Database db, int version) async{
+        await db.execute("CREATE TABLE LICENSE(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, dni TEXT, url TEXT)");
+      }
+    );
+  }
+}
